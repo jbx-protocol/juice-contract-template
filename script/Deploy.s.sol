b03b335cdd72781e9af77adc34b79aabfefae118
jango <77952627+mejango@users.noreply.github.com>
2023-07-10 13:08:50 -0300

    Update Deploy.s.sol
    deploy script refactor

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,18 +3,28 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 
-contract DeployMainnet is Script {
-    function setUp() public {}
-
-    function run() public {
+contract Deploy is Script {
+    // TODO: Add params that are specific to each environment.
+    function _run() internal {
         vm.broadcast();
+        // TODO: Add logic that should run the same between all environments.
     }
 }
 
-contract DeployGoerli is Script {
+contract DeployMainnet is Deploy {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        _run(); // TODO: Pass in param that are specific to the Mainnet environment.
+    }
+}
+
+contract DeployGoerli is Deploy {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+        _run(); // TODO: Pass in param that are specific to the Mainnet environment.
     }
 }
